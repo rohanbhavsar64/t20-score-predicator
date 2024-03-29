e3e1052f26cb8cdbf6d4816b908bd2273f821109
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-03-29 15:48:17 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy as np
-
+from streamlit_lottie import st_lottie
 final_df = pd.read_csv('match.csv')
 final_df=final_df.drop(columns=['Unnamed: 0'])
 # st.write(df)
@@ -25,7 +25,7 @@ scaler=StandardScaler()
 pipe = Pipeline(steps=[
     ('step1',trf),
     ('step2',StandardScaler()),
-    ('step3', LinearRegression())
+    ('step3', DecisionTreeRegressor())
 ])
 pipe.fit(X_train,y_train)
 y_pred = pipe.predict(X_test)
@@ -77,11 +77,40 @@ with col2:
     i=st.selectbox('Wickets in last three overs',sorted(wic))
 with col3:
     e= st.number_input('balls left in Inning')
-#input_df = pd.DataFrame(
-  #  {'name': [name], 'company': [company], 'year': [year], 'kms_driven': [kms_driven], 'fuel_type': [fuel_type]})
 n = pipe.predict(pd.DataFrame(columns=['battingTeam','bowlingTeam','city','Score','ball_left','wickets','crr','last_five','last_five_wickets'],
                               data=np.array([a,b,c,d,e,f,g,h,i]).reshape(1, 9)))
-# result = pipe.predict(input_df)
-
-if st.button('Predict'):
+import plotly.express as px
+if st.button('Prediction & Analysis') and a!=b:
     st.subheader('Predicted Score is:'+str(int(n[0])))
+    m=final_df[final_df['city']==c]['runs_x'].mean()
+    data1=np.array([(int(n[0]),'predicted score'),(int(m),'Average score')])
+    df=pd.DataFrame(data1,columns=['runs',' '])
+    a1=final_df[final_df['battingTeam']==a][final_df[final_df['battingTeam']==a]['ball_left']==e]['Score'].mean()
+    b1=final_df[final_df['battingTeam']==a][final_df[final_df['battingTeam']==a]['ball_left']==0]['Score'].mean()
+    s1 = final_df[final_df['city'] == c][final_df[final_df['city'] == c]['ball_left'] == e]['Score'].mean()
+    s2 = final_df[final_df['city'] == c][final_df[final_df['city'] == c]['ball_left'] == 0]['Score'].mean()
+    x1=final_df[final_df['bowlingTeam']==b][final_df[final_df['bowlingTeam']==b]['ball_left']==e]['Score'].mean()
+    x2=final_df[final_df['bowlingTeam'] == b][final_df[final_df['bowlingTeam'] == b]['ball_left'] == 0]['Score'].mean()
+    fig=px.bar(df,x=' ',y='runs',color_discrete_sequence=['purple','Red'],title='Avg.Total & Predicted Total ')
+    st.write(fig)
+    df1 = final_df[final_df['battingTeam'] == a][final_df[final_df['battingTeam'] == a]['ball_left'] == 0]
+    fig1=px.scatter(df1[df1['bowlingTeam']==b].tail(10),y='Score',color_discrete_sequence=['purple'],title='Totals Against '+b)
+    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', xaxis_title='matches')
+    fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
+    fig1.update_traces(marker=dict(size=10,
+                                   line=dict(width=2,
+                                             color='rgba(135, 206, 250, 0.5)')),
+                       selector=dict(mode='markers'))
+    fig1.update_xaxes(showticklabels=False)
+    st.write(fig1)
+    df2=final_df[final_df['city'] == c][final_df[final_df['city'] == c]['ball_left'] == 0]
+    fig2 = px.line(df2,y='Score', color_discrete_sequence=['Purple'],title='Satuts of Ground in Previous match')
+    st.write(fig2)
+    st.subheader('facts')
+    st.write('Avg. Runs Scored by '+a+' in last '+str(e/6)+' overs is: '+str(int(b1-a1)))
+    st.write('Avg. Runs given by '+b+' in last '+str(e/6)+' overs is: '+str(int(x2-x1)))
+    st.write('Avg. Runs Scored in ' + c + ' in last ' + str(e / 6) + ' overs is: ' + str(int(s2 - s1)))
+
+
+
+
