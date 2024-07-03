853c6f547ef71b0fa38cb8241179eb872c4b38ac
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-03 11:18:08 +0530

    Update win.py

--- win.py
+++ win.py
@@ -108,4 +108,5 @@ batting_team=fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win']
 bowling_team=fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['lose'], mode='lines', line=dict(color='red', width=4)))
 runs=fig.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over']))
 fig.update_layout(title='Target-' + str(target))
-fig.show()
+import streamlit as st
+st.write(fig)
