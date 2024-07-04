5d62d4bb54f89f0673ccff362936e7832216edc0
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-04 15:25:05 +0530

    Update scratchpad.py

--- scratchpad.py
+++ scratchpad.py
@@ -110,4 +110,38 @@ pipe=Pipeline(
     ])
 
 pipe.fit(xtrain,ytrain)
+def match_progression(x_df,Id,pipe):
+    match = x_df[x_df['match_id'] ==Id]
+    match = match[(match['ball'] == 6)]
+    temp_df = match[['battingTeam_x','bowlingTeam_x','city_y','runs_left','balls_left','wickets','runs_x_y','crr','rrr','winner','last_five_runs']].fillna(0)
+    temp_df = temp_df[temp_df['balls_left'] != 0]
+    if temp_df.empty:
+        print("Error: Match is not Existed")
+        a=1
+        return None, None
+    result = pipe.predict_proba(temp_df)
+    temp_df['lose'] = np.round(result.T[0]*100,1)
+    temp_df['win'] = np.round(result.T[1]*100,1)
+    temp_df['end_of_over'] = range(1,temp_df.shape[0]+1)
+    
+    target = temp_df['runs_x_y'].values[0]
+    runs = list(temp_df['runs_left'].values)
+    new_runs = runs[:]
+    runs.insert(0,target)
+    temp_df['runs_after_over'] = np.array(runs)[:-1] - np.array(new_runs)
+    wickets = list(temp_df['wickets'].values)
+    new_wickets = wickets[:]
+    new_wickets.insert(0,10)
+    wickets.append(0)
+    w = np.array(wickets)
+    nw = np.array(new_wickets)
+    temp_df['wickets_in_over'] = (nw - w)[0:temp_df.shape[0]]
+    
+    print("Target-",target)
+    temp_df = temp_df[['end_of_over','runs_after_over','wickets_in_over','lose','win']]
+    return temp_df,target
+temp_df,target = match_progression(match_df,'1000',pipe)
+temp_df
+import streamlit as st
+st.write(temp_df)
 
