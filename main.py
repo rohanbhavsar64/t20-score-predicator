f3505b0b74bf5a8f3330be9940a17345bd12a8af
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-10-26 18:04:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,146 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy as np
+o=st.number_input('Over No.(Not Greater Than Overs Played in 2nd Innings)') or 20
+h = st.text_input('URL( ESPN CRICINFO >Select Match > Click On Overs )') or 'https://www.espncricinfo.com/series/india-in-west-indies-2023-1381201/west-indies-vs-india-1st-t20i-1381217/match-overs-comparison'
+if (h=='https://www.espncricinfo.com/series/india-in-west-indies-2023-1381201/west-indies-vs-india-1st-t20i-1381217/match-overs-comparison'):
+    st.write('Enter Your URL')
+r = requests.get(h)
+#r1=requests.get('https://www.espncricinfo.com/series/icc-cricket-world-cup-2023-24-1367856/india-vs-new-zealand-1st-semi-final-1384437/full-scorecard')
+b=BeautifulSoup(r.text,'html')
+venue=b.find(class_='ds-flex ds-items-center').text.split(',')[1]
+list=[]
+list1=[]
+list2=[]
+list3=[]
+list4=[]
+list5=[]
+list6=[]
+list7=[]
+list8=[]
+list9=[]
+list10=[]
+#print(b.find_all(class_='ds-text-tight-s ds-font-regular ds-flex ds-justify-center ds-items-center ds-w-7 ds-h-7 ds-rounded-full ds-border ds-border-ui-stroke ds-bg-fill-content-prime')[49].text)
+elements = b.find_all(class_='ds-cursor-pointer ds-pt-1')
+for i, element in enumerate(elements):
+    if not element.text.split('/'):
+        print(' ')
+    else:
+        if i % 2 != 0:
+            list.append(element.text.split('/')[0])
+            list1.append(element.text.split('/')[1].split('(')[0])
+for i, element in enumerate(elements):
+    if element.text.split('/') is None:
+        print(' ')
+    else:
+        if i % 2 == 0:
+            list8.append(element.text.split('/')[0])
+            list9.append(i/2+1)
+            list10.append(element.text.split('/')[1].split('(')[0])
+            
+dict1={'inng1':list8,'over':list9,'wickets':list10}
+df1=pd.DataFrame(dict1)
+for i in range(len(list)):
+    list2.append(b.find_all(class_='ds-text-tight-s ds-font-regular ds-flex ds-justify-center ds-items-center ds-w-7 ds-h-7 ds-rounded-full ds-border ds-border-ui-stroke ds-bg-fill-content-prime')[i].text)
+    list3.append(b.find(class_='ds-text-compact-m ds-text-typo ds-text-right ds-whitespace-nowrap').text.split('/')[0])
+    list4.append(b.find_all('th',class_='ds-min-w-max')[1].text)
+    list5.append(b.find_all('th',class_='ds-min-w-max')[2].text)
+    list6.append(b.find(class_='ds-flex ds-items-center').text.split(',')[1])
+    if o==20:
+        list7.append(b.find(class_='ds-text-tight-s ds-font-medium ds-truncate ds-text-typo').text.split(' ')[0])
+if o==20:
+    dict = {'batting_team': list5, 'bowling_team': list4,'venue':list6,'score':list,'wickets':list1,'over':list2,'target':list3,'winner':list7} 
+else:
+    dict = {'batting_team': list5, 'bowling_team': list4,'venue':list6,'score':list,'wickets':list1,'over':list2,'target':list3} 
+df=pd.DataFrame(dict)
+
+df['score']=df['score'].astype('int')
+df1['inng1']=df1['inng1'].astype('int')
+df1['over']=df1['over'].astype('int')
+df['over']=df['over'].astype('int')
+df['wickets']=df['wickets'].astype('int')
+df['target']=df['target'].astype('int')
+df['runs_left']=df['target']-df['score']
+df=df[df['score']<df['target']]
+df['crr']=(df['score']/df['over'])
+df['rrr']=((df['target']-df['score'])/(20-df['over']))
+df['balls_left']=120-(df['over']*6)
+df['runs'] = df['score'].diff()
+df['last_10']=df['runs'].rolling(window=10).sum()
+df['wickets_in_over'] = df['wickets'].diff()
+df['last_10_wicket']=df['wickets_in_over'].rolling(window=10).sum()
+df=df.fillna(20)
+#st.write(df)
+df['match_id']=100001
+neg_idx = df1[df1['inng1']<0].diff().index
+if not neg_idx.empty:
+    df1 = df1[:neg_idx[0]]
+lf=df
+lf=lf[:int(o)]
+st.subheader('Scorecard')
+o=int(o)
+if o != 50:
+    # Create a single row with two columns
+    col1, col2 = st.columns([1, 1])  # Equal width columns
+
+    with col1:
+        bowling_team = df['bowling_team'].unique()[0]
+        batting_team = df['batting_team'].unique()[0]
+
+        # Get the URL for the bowling team
+        bowling_team_url = sf[sf['Country'] == bowling_team]['URL']
+        if not bowling_team_url.empty:
+            # Display the bowling team flag and name in the same line
+            col_bowling, col_bowling_name = st.columns([1, 3])  # Adjust proportions as needed
+            with col_bowling:
+                st.image(bowling_team_url.values[0], width=50)  # Adjust width as needed
+            with col_bowling_name:
+                st.write(f"**{bowling_team}**")
+
+        # Get the URL for the batting team
+        batting_team_url = sf[sf['Country'] == batting_team]['URL']
+        if not batting_team_url.empty:
+            # Display the batting team flag and name in the same line
+            col_batting, col_batting_name = st.columns([1, 3])  # Adjust proportions as needed
+            with col_batting:
+                st.image(batting_team_url.values[0], width=50)  # Adjust width as needed
+            with col_batting_name:
+                st.write(f"**{batting_team}**")
+
+    with col2:
+        # Adjust the layout of col2 to be left-aligned
+        st.markdown("<div style='text-align: right;'>", unsafe_allow_html=True)  # Ensure left alignment
+        st.write(str(df['target'].unique()[0]) + '/' + str(df1.iloc[-1, 2]))
+        st.write('(' + str(df.iloc[o - 1, 5]) + '/' + '20)' + '    ' + str(df.iloc[o - 1, 3]) + '/' + str(df.iloc[o - 1, 4]))
+        st.text('crr : ' + str(df.iloc[o - 1, 8].round(2)) + '  rrr : ' + str(df.iloc[o - 1, 9].round(2)))
+        st.write(batting_team + ' Required ' + str(df.iloc[o - 1, 7]) + ' runs in ' + str(df.iloc[o - 1, 10]) + ' balls')
+        st.markdown("</div>", unsafe_allow_html=True)  # Close the div for left alignment
+
+    # Display teams and results
+else:
+  col1, col2 = st.columns(2)
+  with col1:
+    st.write(f"**{df['bowling_team'].unique()[0]}**")
+    st.write(f"**{df['batting_team'].unique()[0]}**")
+  with col2:
+    st.write(str(df['target'].unique()[0]))
+    st.write('(' + str(df.iloc[-1, 5]) + '/' + '20)   ' + str(df.iloc[-1, 3]) + '/' + str(df.iloc[-1, 4]))
+
+  if 'winner' in df.columns and not df['winner'].empty:
+    winner = df['winner'].unique()
+    if len(winner) > 0:
+      st.write(winner[0] + ' Won')
+    else:
+      st.write("Winner information not available.")
+import plotly.graph_objects as go
+fig = go.Figure(data=[
+    go.Scatter(x=df1['over'], y=df1['inng1'],line_width=3,line_color='red',name=df['bowling_team'].unique()[0]),
+    go.Scatter(x=lf['over'], y=lf['score'],line_width=3,line_color='green',name=df['batting_team'].unique()[0])
+])
+fig.update_layout(title='Score Comperison',
+                  xaxis_title='Over',
+                  yaxis_title='Score')
+gf=df
 final_df = pd.read_csv('match.csv')
 final_df=final_df.drop(columns=['Unnamed: 0'])
 # st.write(df)
