e2c3c6cd772d0a244805abd10592215fea0e3988
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-12-14 12:58:42 +0530

    Update porn.py

--- porn.py
+++ porn.py
@@ -5,8 +5,8 @@ import streamlit as st
 df = pd.read_csv('pornstars_0.csv')
 a=st.selectbox('Name',df['0'].unique())
 # Assuming the image path is in the column named '5' and the first row
-image_path = df[df['0']==a]['5']
+image_path = df[df['0']==a]['5'][0]
 
 # Display the image with the updated parameter
-#st.image(image_path, caption='Image from DataFrame', use_container_width=True)
+st.image(image_path, caption='Image from DataFrame', use_container_width=True)
 st.write(image_path)
