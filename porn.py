0b2e38f6a086e4a5329923c6a55a249ca409faca
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-12-14 12:55:39 +0530

    Update porn.py

--- porn.py
+++ porn.py
@@ -6,6 +6,7 @@ df = pd.read_csv('pornstars_0.csv')
 
 # Assuming the image path is in the column named '5' and the first row
 image_path = df['5'][5]
+st.selectbox('Name',df['0'].unique())
 
 # Display the image with the updated parameter
 st.image(image_path, caption='Image from DataFrame', use_container_width=True)
