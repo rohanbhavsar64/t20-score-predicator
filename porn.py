fb23d7f2fe74e80f6fdc1a4b9a935f6f9a1cfd4e
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-12-14 12:36:47 +0530

    Update porn.py

--- porn.py
+++ porn.py
@@ -5,7 +5,7 @@ import streamlit as st
 df = pd.read_csv('pornstars_0.csv')
 
 # Assuming the image path is in the column named '5' and the first row
-image_path = df['5'][0]
+image_path = df['5'][5]
 
 # Display the image with the updated parameter
 st.image(image_path, caption='Image from DataFrame', use_container_width=True)
