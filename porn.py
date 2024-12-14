cd6ce927cdc0cd319605aa3cbb8d8a829768f719
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-12-14 12:35:02 +0530

    Update porn.py

--- porn.py
+++ porn.py
@@ -7,5 +7,5 @@ df = pd.read_csv('pornstars_0.csv')
 # Assuming the image path is in the column named '5' and the first row
 image_path = df['5'][0]
 
-# Display the image
-st.image(image_path, caption='Image from DataFrame', use_column_width=True)
+# Display the image with the updated parameter
+st.image(image_path, caption='Image from DataFrame', use_container_width=True)
