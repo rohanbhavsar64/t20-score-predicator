1b8c45351716aa91e5bac6ce3a0e0372fb13076b
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-12-14 12:58:12 +0530

    Update porn.py

--- porn.py
+++ porn.py
@@ -8,5 +8,5 @@ a=st.selectbox('Name',df['0'].unique())
 image_path = df[df['0']==a]['5']
 
 # Display the image with the updated parameter
-st.image(image_path, caption='Image from DataFrame', use_container_width=True)
+#st.image(image_path, caption='Image from DataFrame', use_container_width=True)
 st.write(image_path)
