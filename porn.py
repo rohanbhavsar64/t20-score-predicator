6b1910cfb2475ee270350645d46acc54bc8632e0
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-12-14 22:24:45 +0530

    Update porn.py

--- porn.py
+++ porn.py
@@ -21,12 +21,21 @@ desc=df[df['0'] == a]['8'].values[0]
 st.write(desc)
 x=df['8']
 y=df['0']
-
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=1)
 model = keras.models.Sequential()
 model.add(keras.layers.Flatten(input_shape=[28,28]))
 model.add(keras.layers.Dense(300, activation = "relu"))
 model.add(keras.layers.Dense(100, activation = "relu"))
 model.add(keras.layers.Dense(10, activation = "softmax"))
+history = model.fit(X_train, y_train, epochs=30, batch_size=32)
+import pandas as pd
+
+pd.DataFrame(history.history).plot(figsize=(15,8))
+plt.grid(True)
+plt.gca().set_ylim(0,1)
+st.write(plt.show())
+
 
 
 
