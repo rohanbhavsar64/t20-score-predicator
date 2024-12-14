b50096b2731b9d49a0a8e3dbe8d4aab633996f3f
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-12-14 12:56:19 +0530

    Update porn.py

--- porn.py
+++ porn.py
@@ -5,7 +5,7 @@ import streamlit as st
 df = pd.read_csv('pornstars_0.csv')
 
 # Assuming the image path is in the column named '5' and the first row
-image_path = df['5'][5]
+image_path = df['0']['5'].unique()
 st.selectbox('Name',df['0'].unique())
 
 # Display the image with the updated parameter
