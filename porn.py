1fc0c8399cc47891e3a6a03044c224ffd9911436
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-12-14 12:34:17 +0530

    Update porn.py

--- porn.py
+++ porn.py
@@ -1,4 +1,11 @@
 import pandas as pd
 import streamlit as st
-df=pd.read_csv('pornstars_0.csv')
-st.image(df['5'][0])
+
+# Load the CSV file
+df = pd.read_csv('pornstars_0.csv')
+
+# Assuming the image path is in the column named '5' and the first row
+image_path = df['5'][0]
+
+# Display the image
+st.image(image_path, caption='Image from DataFrame', use_column_width=True)
