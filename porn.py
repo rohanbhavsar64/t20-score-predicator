f32e191e6f90588c90550a94941914a9c7b450a4
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-12-21 10:31:18 +0530

    Update porn.py

--- porn.py
+++ porn.py
@@ -5,7 +5,7 @@ from tensorflow import keras
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
-
+url='https://www.sexvid.pro/pornstars/'
 # Load the CSV file
 df = pd.read_csv('pornstars_0.csv')
 
@@ -14,7 +14,7 @@ a = st.selectbox('Name', df['0'].unique())
 
 # Get the image path for the selected name
 image_path = df[df['0'] == a]['5'].values[0]  # Use .values[0] to get the first element
-image_path1 = 'https://www.youtube.com/watch?v=urJP8gsj8vI'  # Use .values[0] to get the first element
+image_path1 = f'https://www.sexvid.pro/pornstars/{a}/'  # Use .values[0] to get the first element
 
 # Display the image
 st.image(image_path, caption=f'Image of {a}', use_container_width=True)
