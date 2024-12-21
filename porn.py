9e9b84797ef7c14e9ce400409e31852f0bcdbe0f
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-12-21 10:54:56 +0530

    Update porn.py

--- porn.py
+++ porn.py
@@ -32,4 +32,4 @@ y = df['0']    # Target variable
 video_url = "https://cdn1.sexvid.pro/contents/videos_screenshots/9000/9400/284x160/3.jpg"  # Replace with actual video URL
 
 # Display the video
-st.video(video_url)
+st.video(video_url.mp4)
